14964fcab1514a3583f39b64e5cf4fe65ff1abb0
Zach-Trani <trani.zach@gmail.com>
2023-03-15 18:52:37 -0400

    feat: add text and number inputs as a proof of concept for the DB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+/**
+ *
+ *
+ * @return {*}
+ */
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <div>This is the Home Page!</div>
+
+      {/* use a form to package inputs together as one unit */}
+      {/* use a post method to 'Create something new' like in CRUD */}
+      {/* <form action="/new" method="POST">
+          <label for="textinput">Input Text</label>   <!-- labels are a good practice for input! -->
+          <input type="text" name="textinput">    <!-- input needs a name to display in the database, will not show otherwise -->
+          <br>
+          <label for="numinput">Input Number</label>
+          <input type="number" name="numinput" value=0>
+          <br>
+          <button type="submit">Add item</button>
+      </form> */}
+
+      <form action="/new" method="POST">
+        <label for="textinput">Input Text</label>
+        <input type="text" name="textinput"></input>
+        <br></br>
+        <label for="numinput">Input Number</label>
+        <input type="number" name="numinput" value={0}></input>
+        <br></br>
+        <button type="submit">Add item</button>
+      </form>
+
+      {/* <ul>
+        {itemList.map((item) => (
+          <li key={item._id}>
+            <div>{item.textinput}</div>
+            <div>{item.numinput}</div>
+            <a href={`/edit/${item._id}`}>Edit</a>
+            <a href={`/edit/remove/${item._id}`}>Remove</a>
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
 }
 
 export default App;
